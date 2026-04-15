e59786305934d93cde813d6b1ce4fa09db731075
yaroslav8765 <pechorkin2014@gmail.com>
2026-04-15 12:36:09 +0300

    progress[10]

--- types.ts
+++ types.ts
@@ -1,5 +1,24 @@
 import  {type PluginsCommonOptions } from "adminforth";
 import { type CompletionAdapter } from "adminforth";
+
+interface ISessionResource {
+  resource_id: string;
+  id_field: string;
+  title_field: string;
+  turns_field: string;
+  asker_id_field: string;
+  created_at_field: string;
+}
+
+interface ITurnResource {
+  resource_id: string;
+  id_field: string;
+  session_id_field: string;
+  created_at_field: string;
+  prompt_field: string;
+  response_field: string;
+}
+
 export interface PluginOptions extends PluginsCommonOptions {
   /**
    * Adapter instance that will be used to generate responses. 
@@ -19,4 +38,7 @@ export interface PluginOptions extends PluginsCommonOptions {
    * Default is low
    */
   reasoning?: "none" | "minimal" | "low" | "medium" | "high" | "xhigh";
+
+  sessionResource: ISessionResource;
+  // turnResource: ITurnResource;
 }
