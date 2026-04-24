2c4186a9049d9cdce61f6a32fd890148934fcc8c
yaroslav8765 <pechorkin2014@gmail.com>
2026-04-24 12:58:05 +0300

    Show tool title instead of tool skill name

--- custom/conversation_area/ToolRenderer.vue
+++ custom/conversation_area/ToolRenderer.vue
@@ -30,7 +30,7 @@
           <span v-if="props.data?.toolInfo?.durationMs" class="text-xs">({{ (props.data.toolInfo.durationMs / 1000).toFixed(2) }}s)</span>
         </p> -->
         <p class="break-all font-mono text-sm leading-5">
-          {{ props.data?.toolInfo?.toolName }}
+          {{ props.data?.toolInfo?.toolInfo }}
         </p>
       </div>
       <IconAngleDownOutline
