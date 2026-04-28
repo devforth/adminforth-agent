fc3b67b0cbaa8fc130bc9764db4023a19ad53bfb
yaroslav8765 <pechorkin2014@gmail.com>
2026-04-28 14:00:59 +0300

    fix: update CustomAutoScrollContainer and ReasoningRenderer to improve scroll behavior and styling

--- custom/conversation_area/TextRenderer.vue
+++ custom/conversation_area/TextRenderer.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    class="max-w-[80%] flex px-4 m-2 rounded-xl border border-gray-200 dark:border-gray-700"
+    class="flex px-4 m-2 rounded-xl border border-gray-200 dark:border-gray-700"
     @click="handleMarkdownLinkClick"
     :class="[
       hasVegaLite ? 'w-full' : '',
-      props.role === 'user' ? 'bg-lightListTableHeading dark:bg-darkListTableHeading self-end' 
+      props.role === 'user' ? 'bg-lightListTableHeading dark:bg-darkListTableHeading self-end max-w-[80%]' 
         : 'border-none self-start'
     ]"
   >
