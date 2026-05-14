25be973a9a4ebc25dda56aede22e13a1ae6ac3d5
yaroslav8765 <pechorkin2014@gmail.com>
2026-05-14 09:39:35 +0300

    fix: change import of DefaultChatTransport to static import

--- custom/composables/agentStore/useAgentChat.ts
+++ custom/composables/agentStore/useAgentChat.ts
@@ -1,8 +1,8 @@
-// import { DefaultChatTransport } from 'ai';
+import { DefaultChatTransport } from 'ai';
 import { shallowRef, type Ref } from 'vue';
 import { Chat } from '../../chat';
 import { getCurrentPageContext } from './pageContext';
-const { DefaultChatTransport } = await import('ai');
+// const { DefaultChatTransport } = await import('ai');
 
 type AgentImportMeta = ImportMeta & {
   env: {
